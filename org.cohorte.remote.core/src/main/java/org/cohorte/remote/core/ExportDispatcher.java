947e5010868a940498aef3efa8f996e0bc63cd4e
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-02-05 13:09:57 +0100

    Small refactoring
    ExportDispatcher and ImportsRegistry are in the same package now

--- org.cohorte.remote.core/src/main/java/org/cohorte/remote/core/ExportDispatcher.java
+++ org.cohorte.remote.core/src/main/java/org/cohorte/remote/core/ExportDispatcher.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cohorte.remote.exporter;
+package org.cohorte.remote.core;
 
 import java.util.Arrays;
 import java.util.Collection;
